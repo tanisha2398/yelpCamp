4a5f32a24ba70250aeae33bf9267db76b0277a8e
tanisha2398 <tanishanegipro@gmail.com>
2019-05-25 01:50:29 +0530

    update and edit comment

--- routes/comments.js
+++ routes/comments.js
@@ -46,6 +46,29 @@ router.post("/",isLoggedIn,(req,res)=>{
   
 });
 
+//comment edit route
+router.get("/:comment_id/edit",(req,res)=>{
+    
+    Comment.findById(req.params.comment_id,(err,foundComment)=>{
+        if(err){
+            res.redirect("back");
+        }else{
+            res.render("comments/edit",{campground_id:req.params.id,comment:foundComment});
+        }
+    });
+});
+
+//comment update
+router.put("/:comment_id",(req,res)=>{
+    Comment.findByIdAndUpdate(req.params.comment_id,req.body.comment,(err,updatedComment)=>{
+        if(err){
+            res.redirect("back");
+        }else{
+            res.redirect("/campground/"+req.params.id);
+        }
+    });
+});
+
 //middleware
 
 function isLoggedIn(req,res,next){
